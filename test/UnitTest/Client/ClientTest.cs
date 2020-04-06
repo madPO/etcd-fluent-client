a788be78606e90e8ede0c1e3456367e874bf1502
madPO <madpo.s.a@gmail.com>
2020-04-06 21:04:14 +0300

    test: add create client test

--- test/UnitTest/Client/ClientTest.cs
+++ test/UnitTest/Client/ClientTest.cs
@@ -1,6 +1,8 @@
 namespace UnitTest.Client
 {
     using System.Threading;
+    using FluentClient.Auth;
+    using FluentClient.Client;
     using Xunit;
 
     public class ClientTest
@@ -8,7 +10,20 @@ namespace UnitTest.Client
         [Fact]
         public void Create()
         {
-            var client = new EtcdClient("loclahost:2379");
+            var client = new EtcdClient(new [] { "localhost:2379" });
+            
+            using (client)
+            {
+                Thread.Sleep(10);
+            }
+        }
+
+        [Fact]
+        public void UseUserAuth()
+        {
+            var client = new EtcdClient(new [] { "localhost:2379" })
+                .UseAuth(new UserLoginAuth("root", "root"));
+            
             using (client)
             {
                 Thread.Sleep(10);
