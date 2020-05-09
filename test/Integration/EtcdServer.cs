e50dd8799e69eee414e8264f7154304caa6b6a0b
madPO <madpo.s.a@gmail.com>
2020-05-09 19:17:32 +0300

    feat: add docker etcd server

--- test/Integration/EtcdServer.cs
+++ test/Integration/EtcdServer.cs
@@ -7,33 +7,64 @@ namespace Integration
 
     public class EtcdServer : IDisposable
     {
-        private Process _process;
+        private readonly string _rootPassword;
         
-        private TaskCompletionSource<bool> _taskSourcing;
+        private readonly Process _process;
+        
+        private readonly TaskCompletionSource<bool> _taskSourcing;
 
-        public EtcdServer()
+        private ServerState _state;
+
+        private readonly string _name;
+
+        public EtcdServer(string rootPassword = null)
         {
+            _rootPassword = rootPassword;
             _process = new Process();
             _taskSourcing = new TaskCompletionSource<bool>();
+            _state = ServerState.Empty;
+            _name = $"etcd_test_server_{Guid.NewGuid():N}";
         }
         
         public void Run(CancellationToken cancellationToken = default)
         {
             const string controlLine = "ready to serve client requests";
             cancellationToken.ThrowIfCancellationRequested();
+            var cancelSource = new CancellationTokenSource(TimeSpan.FromMinutes(3));
+            cancelSource.Token.Register(() =>
+            {
+                if(_state == ServerState.Starting)
+                    _taskSourcing.SetCanceled();
+            });
+            cancellationToken.Register(() =>
+            {
+                if(_state == ServerState.Starting)
+                    _taskSourcing.SetCanceled();
+            });
+
+            var arguments = "run --rm --publish 2379:2379 --publish 2380:2380";
+
+            if (string.IsNullOrEmpty(_rootPassword))
+            {
+                arguments += " --env ALLOW_NONE_AUTHENTICATION=yes ";
+            }
+            else
+            {
+                arguments += $" --env ETCD_ROOT_PASSWORD={_rootPassword} ";
+            }
+
+            arguments += $"--name {_name}  bitnami/etcd:3";
             
             _process.StartInfo.FileName = "docker.exe";
-            //todo: ALLOW_NONE_AUTHENTICATION in method arguments
-            _process.StartInfo.Arguments = "run --rm --publish 2379:2379 --publish 2380:2380 --env ALLOW_NONE_AUTHENTICATION=yes bitnami/etcd:3";
+            _process.StartInfo.Arguments = arguments;
             _process.StartInfo.UseShellExecute = false;
             _process.StartInfo.RedirectStandardOutput = true;
             _process.StartInfo.RedirectStandardError = true;
+            _process.StartInfo.RedirectStandardInput = true;
             _process.StartInfo.CreateNoWindow = true;
-            _process.OutputDataReceived += (sender, args) =>
+            
+            _process.ErrorDataReceived += (sender, args) =>
             {
-                if(cancellationToken.IsCancellationRequested)
-                    _taskSourcing.SetCanceled();
-
                 var data = args.Data;
                 
                 if(data == null)
@@ -41,27 +72,45 @@ namespace Integration
                 
                 if (data.Contains(controlLine, StringComparison.InvariantCultureIgnoreCase))
                 {
+                    _state = ServerState.Run;
                     _taskSourcing.SetResult(true);
                 }
+
+                if (data.Contains("error", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    _state = ServerState.Empty;
+                    _taskSourcing.SetException(new Exception(data));
+                }
             };
+
+            _state = ServerState.Starting;
+            
             _process.Start();
+            _process.BeginOutputReadLine();
+            _process.BeginErrorReadLine();
         }
 
         public Task WaitLaunchEnding(CancellationToken cancellationToken = default)
         {
             cancellationToken.ThrowIfCancellationRequested();
             
-            //todo: work it!
-            _process.BeginOutputReadLine();
-            
-            Thread.Sleep(TimeSpan.FromMinutes(2));
-            
-            return Task.CompletedTask;
+            return _taskSourcing.Task;
         }
 
         public void Stop()
         {
-            _process?.Kill();
+            if(_state == ServerState.Stopping || _state == ServerState.Empty)
+                return;
+
+            _state = ServerState.Stopping;
+            if (_process != null)
+            {
+                _process.Kill();
+                Process.Start("docker.exe", $"stop {_name}");
+                _process.WaitForExit(60 * 1000);
+            }
+            
+            _state = ServerState.Empty;
         }
 
         public void Dispose()
@@ -69,5 +118,16 @@ namespace Integration
             Stop();
             _process?.Dispose();
         }
+    }
+
+    enum ServerState
+    {
+        Empty,
+        
+        Starting,
+        
+        Run,
+        
+        Stopping
     }
 }
